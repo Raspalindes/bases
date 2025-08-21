2486d31f1d8a85fdf7eb31d3501e93a8c348270b
Raspalindes <surimagenmerida@gmail.com>
2025-08-21 21:00:02 +0200

    feat: implement Dragonball page component with character management and routing

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink,
+    RouterLinkActive,
+  ],
   templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
 
