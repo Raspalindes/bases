cad0ca27bcb5406a158785710842069ea4b98f4e
Raspalindes <surimagenmerida@gmail.com>
2025-08-24 19:29:17 +0200

    servicios

--- src/app/pages/dragonball/dragonball-super-page/dragonball-super-page.component.ts
+++ src/app/pages/dragonball/dragonball-super-page/dragonball-super-page.component.ts
@@ -1,12 +1,8 @@
-import { Component, signal } from '@angular/core';
-import { CharacterAddComponent } from "../../../components/dragonball/character-add/character-add.component";
-import { CharacterListComponent } from "../../../components/dragonball/character-list/character-list.component";
-import { Character } from '../../../interfaces/character.interface';
-// interface Character {
-//   id: number;
-//   name: string;
-//   power: number;
-// }
+import { Component, inject } from '@angular/core';
+import { CharacterAddComponent } from '../../../components/dragonball/character-add/character-add.component';
+import { CharacterListComponent } from '../../../components/dragonball/character-list/character-list.component';
+import { DragonballService } from './../../../services/dragonball.service';
+
 @Component({
   selector: 'app-dragonball-page',
   imports: [CharacterListComponent, CharacterAddComponent],
@@ -14,16 +10,5 @@ import { Character } from '../../../interfaces/character.interface';
   styleUrl: './dragonball-super-page.component.css',
 })
 export class DragonballSuperPageComponent {
-  name = signal('');
-  power = signal(0);
-
-  characters = signal<Character[]>([
-    { id: 1, name: 'Goku', power: 9001 },
-    { id: 2, name: 'Vegeta', power: 8000 },
-  ]);
-
-
-  addCharacter(character: Character) {
-    this.characters.update((list) => [...list, character]);
-  }
+  public DragonballService = inject(DragonballService);
 }
